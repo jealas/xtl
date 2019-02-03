27db4bdabd1eb80673f937b9bafeaf74cd9599d4
Jesse Alas <jessealas94@gmail.com>
2019-02-03 00:06:01 -0800

    Implemented support for callable objects/lambdas.

--- include/xtl/delegate.h
+++ include/xtl/delegate.h
@@ -10,7 +10,7 @@ namespace xtl {
         };
 
         typedef ReturnT(*function_ptr_t)(Args...);
-        typedef ReturnT(Void::*member_function_ptr_t)();
+        typedef ReturnT(Void::*member_function_ptr_t)(Args...);
 
         union function_t {
             explicit function_t(function_ptr_t fn_ptr) : function_ptr{fn_ptr} {}
@@ -30,6 +30,10 @@ namespace xtl {
         template <class Callable>
         delegate(Callable &callable, ReturnT (Callable::*member_fn)(Args...))
             : delegate_{&member_function_ptr_delegate<Callable>}, function_{reinterpret_cast<member_function_ptr_t>(member_fn)}, storage_{reinterpret_cast<storage_t>(&callable)} {}
+
+        template <class Callable>
+        delegate(Callable &callable)
+            : delegate_{&member_function_ptr_delegate<Callable>}, function_{reinterpret_cast<member_function_ptr_t>(&Callable::operator())}, storage_(reinterpret_cast<storage_t>(&callable)) {}
 
         ReturnT operator()(Args... args) const {
             return delegate_(function_, storage_, args...);
--- test/test_delegate.cpp
+++ test/test_delegate.cpp
@@ -17,6 +17,14 @@ struct member_fn {
 
 using predicate = xtl::delegate<bool()>;
 
+struct callable {
+    bool value;
+
+    bool operator()() const {
+        return value;
+    }
+};
+
 TEST_CASE("Test delegate can instantiated", "[xtl][delegate]") {
     SECTION("C++ functions") {
         predicate true_function{true_fn};
@@ -36,6 +44,32 @@ TEST_CASE("Test delegate can instantiated", "[xtl][delegate]") {
         REQUIRE(true_function());
 
         predicate false_function{member, &member_fn::false_call};
+        REQUIRE(!false_function());
+    }
+
+    SECTION("Callable class") {
+        callable true_callable{true};
+        predicate true_function{true_callable};
+
+        REQUIRE(true_function());
+
+        callable false_callable{false};
+        predicate false_function{false_callable};
+
+        REQUIRE(!false_function());
+    }
+
+    SECTION("Lambda") {
+        const auto t = true;
+        auto true_lambda = [&]() { return t; };
+
+        predicate true_function{true_lambda};
+        REQUIRE(true_function());
+
+        const auto f = false;
+        auto false_lambda = [&]() { return f; };
+
+        predicate false_function{false_lambda};
         REQUIRE(!false_function());
     }
 }
