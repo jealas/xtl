2244fdeed4be67aa5e547105a88a5707305b8b7c
Jesse Alas <jessealas94@gmail.com>
2019-02-05 22:18:13 -0800

    Implemented better memory ptr.

--- include/xtl/memory_ptr.h
+++ include/xtl/memory_ptr.h
@@ -3,15 +3,60 @@
 #include <stddef.h>
 
 namespace xtl {
-    template <size_t MemoryAddress, class T>
+    template <size_t StartMemoryAddress, size_t EndMemoryAddress, class T>
     struct memory_ptr {
-        ~memory_ptr() noexcept { this->~T(); }
-        memory_ptr() noexcept { this->T(); }
+        static_assert(StartMemoryAddress <= EndMemoryAddress);
+        static_assert(EndMemoryAddress - StartMemoryAddress + 1== sizeof(T));
 
-        template <class ... Args>
-        memory_ptr(Args && ... args) noexcept { this->T(args...); }
+        constexpr memory_ptr() noexcept = default;
+        ~memory_ptr() noexcept = default;
 
-        T& operator*() noexcept { return *reinterpret_cast<T*>(MemoryAddress); }
-        const T& operator*() const noexcept { return *reinterpret_cast<T*>(MemoryAddress); }
+        memory_ptr(const memory_ptr&) = delete;
+        memory_ptr(memory_ptr&&) = delete;
+
+        memory_ptr &operator=(const memory_ptr&) = delete;
+        memory_ptr &operator=(memory_ptr&&) = delete;
+
+        constexpr T* data() { return reinterpret_cast<T*>(StartMemoryAddress); }
+        constexpr T const * data() const { return reinterpret_cast<T*>(StartMemoryAddress); }
+
+        constexpr T* operator->() { return data(); }
+        constexpr T const * operator->() const { return data(); }
+
+        constexpr T& operator*() noexcept { return *data(); }
+        constexpr const T& operator*() const noexcept { return *data(); }
+    };
+
+    template <size_t StartMemoryAddress, size_t EndMemoryAddress, size_t N, class T>
+    struct memory_ptr<StartMemoryAddress, EndMemoryAddress, T[N]> {
+        static_assert(StartMemoryAddress <= EndMemoryAddress);
+        static_assert(EndMemoryAddress - StartMemoryAddress + 1 == sizeof(T[N]));
+
+        constexpr memory_ptr() noexcept = default;
+        ~memory_ptr() noexcept = default;
+
+        memory_ptr(const memory_ptr&) = delete;
+        memory_ptr(memory_ptr&&) = delete;
+
+        memory_ptr &operator=(const memory_ptr&) = delete;
+        memory_ptr &operator=(memory_ptr&&) = delete;
+
+        constexpr T* data() { return reinterpret_cast<T*>(StartMemoryAddress); }
+        constexpr T const * data() const { return reinterpret_cast<T const *>(StartMemoryAddress); }
+
+        constexpr T* begin() { return data(); }
+        constexpr T const * begin() const { return data(); }
+
+        constexpr T* end() { return data() + N; }
+        constexpr T const * end() const { return data() + N; }
+
+        constexpr T* operator->() { return data(); }
+        constexpr T const * operator->() const { return data(); }
+
+        constexpr T& operator*() noexcept { return *data(); }
+        constexpr const T& operator*() const noexcept { return *data(); }
+
+        constexpr T& operator[](const size_t i) noexcept { return *(data() + i); }
+        constexpr const T& operator[](const size_t i) const noexcept { return *(data() + i); }
     };
 }
